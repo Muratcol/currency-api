6f94ce492813c8449f8614dad3a46ffa75973aab
Murat Colyaran <muratcolyaran@yahoo.com.tr>
2020-06-16 19:06:03 +0300

    Currency model updated and Moment module implemented. --- Demo Version

--- models/Currency.js
+++ models/Currency.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const moment = require('moment');
+const { now } = require('moment');
+moment().format();
 
 
 const CurrencySchema = new Schema({
@@ -14,7 +17,7 @@ const CurrencySchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now,
+        default: moment.utc()
     }
 });
 

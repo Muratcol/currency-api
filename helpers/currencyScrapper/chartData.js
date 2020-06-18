7407e511d7b666b6170239949ea820f07055b571
Murat Colyaran <muratcolyaran@yahoo.com.tr>
2020-06-19 02:24:29 +0300

    Chart update --- Demo Version

--- helpers/currencyScrapper/chartData.js
+++ helpers/currencyScrapper/chartData.js
@@ -2,18 +2,17 @@ const asyncErrorWrapper = require("express-async-handler");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const Currency = require("../../models/Currency");
-const CustomError = require('../../helpers/error/CustomError');
+const CustomError = require("../../helpers/error/CustomError");
 let allCurrencies = new Array();
 
 const chartData = asyncErrorWrapper(async (req, res, next) => {
-
   let url = "https://kur.doviz.com/";
   let response;
   allCurrencies = [];
   response = await axios.get(url);
   const $ = await cheerio.load(response.data);
   let denemeler = await $("#currencies tbody tr");
-  for (let i = 1; i <= 3; i++) { 
+  for (let i = 1; i <= 3; i++) {
     const buy = $(`tr:nth-child(${i}) > td:nth-child(2)`).text().trim();
     const sell = $(`tr:nth-child(${i}) > td:nth-child(3)`).text().trim();
     const update = $(`tr:nth-child(${i}) > td:nth-child(7)`).text().trim();
@@ -29,34 +28,23 @@ const chartData = asyncErrorWrapper(async (req, res, next) => {
     euro: allCurrencies[1],
     gbp: allCurrencies[2],
   });
-  
-  // return res.status(200)
-  // .json({
-  //   success:true,
-  //   data: allCurrencies
-  // })
 });
 
 setInterval(() => {
-  chartData()
-}, 60 * 1 * 1000)
-
+  chartData();
+}, 60 * 15 * 1000);
 
 const updateChart = asyncErrorWrapper(async (req, res, next) => {
-
   const chartDatas = await Currency.find();
 
-  if (chartDatas.length === 0) return next(new CustomError("No new data in database", 404))
+  if (chartDatas.length === 0)
+    return next(new CustomError("No new data in database", 404));
 
-  return res.status(200)
-      .json({
-          success: true,
-          data: chartDatas
-      });
-
+  return res.status(200).json({
+    success: true,
+    data: chartDatas,
+  });
 });
-
-
 
 module.exports = {
   updateChart,

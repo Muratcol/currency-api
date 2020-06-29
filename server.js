0e822749527e66ad76e130cbcd59c860a79fb75c
Murat Colyaran <muratcolyaran@yahoo.com.tr>
2020-06-29 23:12:04 +0300

    Chat system developing API --- Demo Version

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const routers = require('./routers');
+const http= require('http');
 const customErrorHandler = require('./middlewares/errors/customErrorHandler');
 const connectDatabase = require('./helpers/database/connectDatabase');
 const path = require('path');
@@ -17,6 +18,13 @@ connectDatabase();
 const app = express();
 // Express - Body Middleware
 app.use(express.json());
+
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+    console.log('New connection made')
+})
 
 
 const PORT = process.env.PORT; // in case deployment
